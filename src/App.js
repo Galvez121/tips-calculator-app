fb9c634c56d189b35d856d2cfc23747fd5699ac0
Juan Galvez <juanjosegalvez2014@gmail.com>
2023-12-12 20:04:34 -0500

    First commit- v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import DollarIconSVG from "./iconDollar.svg";
+import IconPerson from "./icon-person.svg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <CustomTip />
+      <AmountPay />
+    </main>
   );
 }
 
-export default App;
+function CustomTip() {
+  return (
+    <section className="billInformation">
+      <Bill />
+    </section>
+  );
+}
+
+function Bill() {
+  return (
+    <>
+      <div>
+        <h5>Bill</h5>
+        <span className="icon-wrapper">
+          {" "}
+          <img src={DollarIconSVG} alt="Dollar Icon" />
+        </span>
+
+        <input type="text" className="bill_inputs" />
+      </div>
+
+      <div>
+        <h5>Select tip %</h5>
+        <ul>
+          {["5%", "10%", "15%", "20%", "25%"].map((value) => (
+            <li className="buttons_percentages">{value}</li>
+          ))}
+          <input type="text" className="buttons_percentages" />
+        </ul>
+      </div>
+      <div>
+        <h5>Number of people</h5>
+        <span className="icon-wrapper">
+          {" "}
+          <img src={IconPerson} alt="Dollar Icon" />
+        </span>
+        <input type="text" className="bill_inputs" />
+      </div>
+    </>
+  );
+}
+
+function AmountPay() {
+  return (
+    <section className="totalInformation">
+      <TotalAmount />
+    </section>
+  );
+}
+
+function TotalAmount() {
+  return (
+    <>
+      <div className="pay_information">
+        <h6>Tip Amount</h6>
+        <span className="price">$ 30.6</span>
+      </div>
+      <div className="pay_information">
+        <h6>Total</h6>
+        <span className="price">$ 30</span>
+      </div>
+
+      <button className="reset_button">Reset</button>
+    </>
+  );
+}
